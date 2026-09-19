2ddb76c78a42bfddd37ed6cc7532ca05f770b81e
qiancheng <qiancheng817@gmail.com>
2026-09-19 15:13:50 +0800

    feat(android): remove pull-to-refresh (SwipeRefreshLayout)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -74,6 +74,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.google.android.material:material:1.12.0")
 }
